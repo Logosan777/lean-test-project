c8a6358147f400eb4cfb67858f97eb90d51fc8e9
Logosan777 <logos.core.san@gmail.com>
2025-04-29 12:49:52 +0300

    Delete lakefile.lean

--- lakefile.lean
+++
@@ -1,4 +0,0 @@
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "v4.3.0"
-open Lake DSL
-package lean_test_project { description := "Test project to verify Lake and CI setup" }
-@[default_target] lean_lib TestLib { srcDir := "." roots := #[``Test] }
